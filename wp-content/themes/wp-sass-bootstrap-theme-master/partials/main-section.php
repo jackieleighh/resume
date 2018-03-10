9b062c4e87c45e17e9ca33adc1fb3d27232162b3
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 10:49:08 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -9,7 +9,7 @@ $background_image = get_field('background_image', $postID);
 <section id="<?php echo get_the_title(); ?>" class="band main-block" style="background-image: url('<?php echo $background_image; ?>');">
     <div class="band-container">
         <div class="band-inner">
-        	<h1><?php echo $title; ?></h1>
+        	<h1><i class="fas fa-angle-left"></i><?php echo $title; ?><i class="fas fa-angle-right"></i></h1>
         	<h2><?php echo $subtitle; ?></h2>
         </div>
     </div>
