2714b940e53abbe441d13709ee770db0455d0e80
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 10:03:28 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
@@ -6,10 +6,11 @@ $text = get_field('text', $postID);
 $background_color = get_field('background_color', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
-
-<div class="row text-block" style="background-color: <?php echo $background_color; ?>">
-    <?php if($background_image): ?>
-        <div class="background-image" style="background-image: url('<?php echo $background_image; ?>');"></div>
-    <?php endif; ?>
-    <?php echo $text; ?>
-</div>
+<section id="<?php echo get_the_title(); ?>">
+    <div class="row text-block" style="background-color: <?php echo $background_color; ?>">
+        <?php if($background_image): ?>
+            <div class="background-image" style="background-image: url('<?php echo $background_image; ?>');"></div>
+        <?php endif; ?>
+        <?php echo $text; ?>
+    </div>
+</section>
