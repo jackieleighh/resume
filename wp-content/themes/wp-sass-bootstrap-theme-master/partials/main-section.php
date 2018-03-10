e64a21c3c50ec57554e7a13852aac51e20cff979
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 16:29:48 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -7,19 +7,22 @@ $subtitle = get_field('subtitle', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
 
-
-  <div class="parallax-group">
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer fill"></div>
-  </div>
-  <div class="content">
-    <h1><?php echo $title; ?></h1>
-	<h2><?php echo $subtitle; ?></h2>
-  </div>
+<div class="section-wrapper">
+    <section id="<?php echo get_the_title(); ?>" class="block main-block">
+	  <div class="parallax-group">
+	    <div class="layer"></div>
+	    <div class="layer"></div>
+	    <div class="layer"></div>
+	    <div class="layer"></div>
+	    <div class="layer"></div>
+	    <div class="layer fill"></div>
+	  </div>
+	  <div class="content">
+	    <h1><?php echo $title; ?></h1>
+		<h2><?php echo $subtitle; ?></h2>
+	  </div>
+	</section>
+</div>
 
 
 <!-- <div class="section-wrapper">
