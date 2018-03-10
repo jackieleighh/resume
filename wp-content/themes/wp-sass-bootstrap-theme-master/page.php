3e8be27719c030015248392943831830a898fdab
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 09:11:22 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/page.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/page.php
@@ -1,4 +1,22 @@
 <?php get_header(); ?>
+
+    <?php // theloop
+
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+    <?php
+        if (count(get_field('sections')) > 0) {
+            include(locate_template('partials/starter.php'));
+        }
+    ?>
+
+    <?php endwhile; ?>
+    <?php else: ?>
+
+        <?php get_404_template(); ?>
+
+    <?php endif; ?>
+    
     <section class="band band-a">
       <div class="band-container">
         <div class="band-inner">
