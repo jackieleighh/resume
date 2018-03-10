6ac9597e8f717e909520ca06248e36cc701d4040
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 13:35:26 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
@@ -9,7 +9,7 @@ $background_image = get_field('background_image', $postID);
 
 <div class="section-wrapper">
     <section id="<?php echo get_the_title(); ?>" class="block text-block" style="background-color: <?php echo $background_color; ?>">
-        <div class="block-inner">
+        <div class="section-inner">
             <?php echo $text; ?>
         </div>
     </section>
