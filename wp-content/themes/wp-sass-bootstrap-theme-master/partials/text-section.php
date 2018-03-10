906c21784f7a28dfff566b30f6a450bf420a3894
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 15:00:23 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/text-section.php
@@ -5,12 +5,30 @@ $post = get_post($postID);
 $text = get_field('text', $postID);
 $background_color = get_field('background_color', $postID);
 $background_image = get_field('background_image', $postID);
+$type = get_field('type', $postID);
+$image = get_field('image', $postID);
 ?>
 
 <div class="section-wrapper">
     <section id="<?php echo get_the_title(); ?>" class="block text-block" style="background-color: <?php echo $background_color; ?>">
         <div class="section-inner">
-            <?php echo $text; ?>
+            <?php if($type == 'no_image'): ?>
+                <?php echo $text; ?>
+            <?php elseif($type == 'image_left'): ?>
+                <div class="col-sm-4">
+                    <img src="<?php echo $image; ?>"/>
+                </div>
+                <div class="col-sm-8">
+                    <?php echo $text; ?>
+                </div>
+            <?php else: ?>
+                <div class="col-sm-8">
+                    <?php echo $text; ?>
+                </div>
+                <div class="col-sm-4">
+                    <img src="<?php echo $image; ?>"/>
+                </div>
+            <?php endif; ?>
         </div>
     </section>
 </div>
