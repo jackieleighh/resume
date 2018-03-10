ec854a3a78c18b7667a257ae8f7985f77d00bec0
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 10:56:23 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/functions.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/functions.php
@@ -5,7 +5,7 @@ function theme_styles() {
 	wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/bootstrap/css/styles.css' );
 	wp_enqueue_style('print_css', get_template_directory_uri() . '/bootstrap/css/print.css' );
 	wp_enqueue_style('style.css', get_template_directory_uri() . '/css/style.css');
-	wp_register_style('https://use.fontawesome.com/releases/v5.0.8/css/all.css', '','',false);
+	wp_register_style('fontawesome', 'https://use.fontawesome.com/releases/v5.0.8/css/all.css', '','',false);
 }
 
 	add_action('wp_enqueue_scripts', 'theme_styles' );
