9e12db8cdac1b6420c06c5a20cbcabcc4edefc36
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 11:18:48 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/js/custom.js
+++ wp-content/themes/wp-sass-bootstrap-theme-master/js/custom.js
@@ -17,3 +17,23 @@ function sidebarMenu() {
 	  }
 	});
 }
+
+$(window).scroll(function(){\
+  // Add parallax scrolling to all images in .parallax-section container
+  $('.parallax-section').each(function(){
+    // only put top value if the window scroll has gone beyond the top of the image
+		    if ($(this).offset().top < $(window).scrollTop()) {
+			// Get ammount of pixels the image is above the top of the window
+			var difference = $(window).scrollTop() - $(this).offset().top;
+	      // Top value of image is set to half the amount scrolled
+	      // (this gives the illusion of the image scrolling slower than the rest of the page)
+	      var half = (difference / 2) + 'px',
+        transform = 'translate3d( 0, ' + half + ',0)';
+
+	      $(this).find('section').css('transform', transform);
+	} else {
+	      // if image is below the top of the window set top to 0
+			$(this).find('section').css('transform', 'translate3d(0,0,0)');
+    }
+  });
+});
--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -6,14 +6,13 @@ $title = get_field('title', $postID);
 $subtitle = get_field('subtitle', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
-<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="https://pixelcog.github.io/parallax.js/img/stellar-spire-eagle-nebula-1400x900.jpg" data-natural-width="1400" data-natural-height="900"><div class="container">
-<section id="<?php echo get_the_title(); ?>" class="parallax-window main-block" data-parallax="scroll" data-image-src="<?php echo $background_image; ?>">
-    <div class="band-container">
-        <div class="band-inner">
-        	<h1><i class="fas fa-angle-left"></i><?php echo $title; ?><i class="fas fa-angle-right"></i></h1>
-        	<h2><?php echo $subtitle; ?></h2>
-        </div>
-    </div>
-</section>
-</div>
+<div class="parallax-section">
+	<section id="<?php echo get_the_title(); ?>" class="band main-block" style="background-image: url('<?php echo $background_image; ?>');">
+	    <div class="band-container">
+	        <div class="band-inner">
+	        	<h1><i class="fas fa-angle-left"></i><?php echo $title; ?><i class="fas fa-angle-right"></i></h1>
+	        	<h2><?php echo $subtitle; ?></h2>
+	        </div>
+	    </div>
+	</section>
 </div>
