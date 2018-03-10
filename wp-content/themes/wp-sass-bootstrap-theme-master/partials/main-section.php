91a57701121f45b007c53b1cb970a8f5d0596256
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 12:34:42 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -7,23 +7,10 @@ $subtitle = get_field('subtitle', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
 
-<section id="<?php echo get_the_title(); ?>" class="band main-block" style="background-image: url('<?php echo $background_image; ?>');">
-    <div class="band-container">
-        <div class="band-inner">
-        	<h1><i class="fas fa-angle-left"></i><?php echo $title; ?><i class="fas fa-angle-right"></i></h1>
-        	<h2><?php echo $subtitle; ?></h2>
-        </div>
-    </div>
-</section>
-
-<div class="parallax-window">
-  <div class="parallax-slider">
-    <span style="position:absolute; top: 400px; left: 400px;">Some Text</span>
-	<p>Some other Content</p>
-  </div>
-</div>
-
-<div class="block">
-  <img src="<?php echo $background_image; ?>" data-speed="-0.25" class="img-parallax">
-  <h2>Parallax Speed -0.25</h2>
+<section id="<?php echo get_the_title(); ?>" class="block main-block">
+	<img src="<?php echo $background_image; ?>" data-speed="-0.25" class="img-parallax">
+	<div class="block-inner">
+		<h1><i class="fas fa-angle-left"></i><?php echo $title; ?><i class="fas fa-angle-right"></i></h1>
+		<h2><?php echo $subtitle; ?></h2>
+	</div>
 </div>
