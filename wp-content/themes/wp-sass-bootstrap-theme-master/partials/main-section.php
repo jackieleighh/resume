48da60c119a37eb77de69d9e67caee49fa68e03a
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-10 16:25:29 -0500

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -7,6 +7,22 @@ $subtitle = get_field('subtitle', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
 
+<div class="bg"></div>
+<div class="parallax">
+  <div class="parallax-group">
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer"></div>
+    <div class="layer fill"></div>
+  </div>
+  <div class="content">
+    <h1>Parallax Scrolling</h1>
+    <p>Using only CSS</p>
+  </div>
+</div>
+
 <div class="section-wrapper">
 	<section id="<?php echo get_the_title(); ?>" class="block main-block" style="background-image: url('<?php echo $background_image; ?>');">
 		<div class="bg"></div>
