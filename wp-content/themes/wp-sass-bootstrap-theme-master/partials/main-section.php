3b39ab2ea7529d5fc3269c7843368e8d361f4f87
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-13 08:34:06 -0400

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -8,7 +8,7 @@ $background_image = get_field('background_image', $postID);
 ?>
 
 
-<div class="parallax_layer section-wrapper">
+<div class="section-wrapper">
 	<section id="<?php echo get_the_title(); ?>" class="block main-block" style="background-image: url('<?php echo $background_image; ?>');">
 		<div class="section-inner">
 			<h1><?php echo $title; ?></h1>
