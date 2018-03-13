80cffeedb58cacc97996e8ca8da21e4a6e47e6cd
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-12 23:14:33 -0400

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/main-section.php
@@ -7,25 +7,15 @@ $subtitle = get_field('subtitle', $postID);
 $background_image = get_field('background_image', $postID);
 ?>
 
-<!-- <div class="bg"></div> -->
-<div class="parallax">
-  <div class="parallax-group">
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer"></div>
-    <div class="layer fill"></div>
-  </div>
-  <div class="content">
-    <h1>Parallax Scrolling</h1>
-    <p>Using only CSS</p>
-  </div>
+<div class="mountains">
+	<img class="layer road" src="img/road.png" />
+	<img class="layer layer-1" src="img/layer1.png" />
+	<img class="layer layer-2" src="img/layer2.png" />
+	<img class="layer layer-3" src="img/layer3.png" />
+	<img class="layer layer-4" src="img/layer4.png" />
 </div>
-
 <div class="section-wrapper">
 	<section id="<?php echo get_the_title(); ?>" class="block main-block" style="background-image: url('<?php echo $background_image; ?>');">
-		<div class="bg"></div>
 		<div class="section-inner">
 			<h1><?php echo $title; ?></h1>
 			<h2><?php echo $subtitle; ?></h2>
