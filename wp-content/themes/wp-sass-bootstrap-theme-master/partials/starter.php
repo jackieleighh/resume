c17cd7c390c5d2a7cc64c127780fab91fa54b4c0
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-13 22:44:27 -0400

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/starter.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/starter.php
@@ -34,7 +34,7 @@
 			include(locate_template('partials/text-section.php'));
 		} else if ($single['cat_id'] == 'main') {
 			include(locate_template('partials/main-section.php'));
-		} else if ($single['cat_id'] == 'columns') {
+		} else if ($single['cat_id'] == 'column') {
 			include(locate_template('partials/column-section.php'));
 		} 
 	}
