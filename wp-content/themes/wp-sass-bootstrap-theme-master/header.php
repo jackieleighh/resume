a5eea4c00a1e6c30c1148fc8a18fcf81f6657906
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-13 21:31:45 -0400

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/header.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/header.php
@@ -15,35 +15,17 @@
   </head>
 
     <body <?php body_class(); ?>>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul id="pagepiling-menu" class="nav navbar-nav">
-              <?php 
-              $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
-              $menuID = $menuLocations['header_menu']; // Get the *primary* menu ID
-              $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
-              $i = 0;
-              foreach ( $primaryNav as $navItem ) {
-                  echo '<li ';
-                  if($i == 0) echo 'class="active" ';
-                  echo 'data-menuanchor="'.$navItem->url.'"><a href="'.$navItem->url.'" title="'.$navItem->title.'">'.$navItem->title.'</a></li>';
-                  $i++;
-              } ?>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <ul id="pagepiling-menu" class="nav navbar-nav">
+        <?php 
+        $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
+        $menuID = $menuLocations['header_menu']; // Get the *primary* menu ID
+        $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
+        $i = 0;
+        foreach ( $primaryNav as $navItem ) {
+            echo '<li ';
+            if($i == 0) echo 'class="active" ';
+            echo 'data-menuanchor="'.$navItem->url.'"><a href="'.$navItem->url.'" title="'.$navItem->title.'">'.$navItem->title.'</a></li>';
+            $i++;
+        } ?>
+      </ul>
       <div id="pagepiling">
