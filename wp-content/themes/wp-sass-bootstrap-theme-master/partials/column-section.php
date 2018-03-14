4ac752002070bce2e9b199e072526dd5305bc743
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-13 22:46:53 -0400

    side menu

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/column-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/column-section.php
@@ -2,18 +2,18 @@
 
 $postID = $single['id'];
 $post = get_post($postID);
-$title1 = get_field('title1', $postID);
-$image1 = get_field('image1', $postID);
-$description1 = get_field('description1', $postID);
-$title2 = get_field('title2', $postID);
-$image2 = get_field('image2', $postID);
-$description2 = get_field('description2', $postID);
-$title3 = get_field('title3', $postID);
-$image3 = get_field('image3', $postID);
-$description3 = get_field('description3', $postID);
-$title4 = get_field('title4', $postID);
-$image4 = get_field('image4', $postID);
-$description4 = get_field('description4', $postID);
+$title1 = get_field('title_1', $postID);
+$image1 = get_field('image_1', $postID);
+$description1 = get_field('description_1', $postID);
+$title2 = get_field('title_2', $postID);
+$image2 = get_field('image_2', $postID);
+$description2 = get_field('description_2', $postID);
+$title3 = get_field('title_3', $postID);
+$image3 = get_field('image_3', $postID);
+$description3 = get_field('description_3', $postID);
+$title4 = get_field('title_4', $postID);
+$image4 = get_field('image_4', $postID);
+$description4 = get_field('description_4', $postID);
 ?>
 
 <!-- <div class="section-wrapper"> -->
