bfea6e2c5b75c26fcc639d5ff80219e94f249948
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-19 20:00:01 -0400

    post types

---
+++ wp-content/themes/wp-sass-bootstrap-theme-master/lib/portfolio-post-type.php
@@ -0,0 +1,56 @@
+<?php
+// Register Custom Post Type
+function portfolio_post() {
+
+	$labels = array(
+		'name'                  => _x( 'Portfolio Posts', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Portfolio Post', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Portfolio', 'text_domain' ),
+		'name_admin_bar'        => __( 'Portfolio', 'text_domain' ),
+		'archives'              => __( 'Portfolio Archive', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Portfolio', 'text_domain' ),
+		'add_new_item'          => __( 'Add Portfolio', 'text_domain' ),
+		'add_new'               => __( 'Add Portfolio', 'text_domain' ),
+		'new_item'              => __( 'New Portfolio', 'text_domain' ),
+		'edit_item'             => __( 'Edit Portfolio', 'text_domain' ),
+		'update_item'           => __( 'Update Portfolio', 'text_domain' ),
+		'view_item'             => __( 'View Portfolio', 'text_domain' ),
+		'search_items'          => __( 'Search Portfolio', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'portfolio', 'text_domain' ),
+		'description'           => __( 'Portfolio Content', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'thumbnail', 'revisions'),
+		'hierarchical'          => false,
+		//'taxonomies'            => array( 'category' ),
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_icon'				=> 'dashicons-layout',
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'portfolio', $args );
+
+}
+add_action( 'init', 'portfolio_post', 0 );
+?>
