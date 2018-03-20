ff85308ce32db8babf10d51224cf0bfc0d2001ae
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-19 21:21:13 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/lib/skill-post-type.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/lib/skill-post-type.php
@@ -39,7 +39,7 @@ function skill_post() {
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
-		'menu_icon'				=> 'dashicons-layout',
+		'menu_icon'				=> 'dashicons-clipboard',
 		'menu_position'         => 5,
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
