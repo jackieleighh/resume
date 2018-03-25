715f3c872e493f3a0999ba792d617808239b6241
Jacqueline Harnish <jacquelineharnish@Jacquelines-MBP.fios-router.home>
2018-03-25 08:27:21 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/page.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/page.php
@@ -37,7 +37,7 @@
           </div>
         <?php endif; ?>
       </section>
-      
+
       <section id="experience">
         <?php 
         $experience_query = new WP_Query(
@@ -72,24 +72,7 @@
       <?php endwhile; ?>
     </section>
 
-    <!-- <section id="portfolio">
-        <?php 
-        $portfolio_query = new WP_Query(
-          array(
-            'post_type' => 'portfolio',
-            'posts_per_page' => -1,
-            'meta_key' => 'order',
-            'order_by' => 'meta_value',
-            'order' => 'ASC'
-          )
-        );
-        while ( $portfolio_query-> have_posts() ) : $portfolio_query->the_post();  ?>
-
-          <?php include(locate_template('partials/portfolio-section.php')); ?>
-
-        <?php wp_reset_query(); endwhile; ?>
-      <?php endwhile; ?>
-    </section> -->
+    
 
     <?php else: ?>
 
