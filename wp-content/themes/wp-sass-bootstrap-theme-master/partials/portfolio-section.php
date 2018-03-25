ddd1b5ac3dc06b875231df8fc238596235f27137
Jacqueline Harnish <jacquelineharnish@Jacquelines-MBP.fios-router.home>
2018-03-25 08:54:33 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/portfolio-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/portfolio-section.php
@@ -5,7 +5,7 @@
     $link = get_field('link');
 ?>
 
-<div class="col-sm-12 section half portfolio">
+<div class="col-sm-6 section full portfolio">
 	<a href="<?php echo $link; ?>" target="_blank">
 		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 		<div class="row inner">
