6f098d6cb7240825e52f3f8901e52eae035c6f4b
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-28 19:44:51 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/header-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/header-section.php
@@ -6,6 +6,8 @@
 <div class="main" style="background-image: url('<?php echo $image['url']; ?>')">
 	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
 	<div class="row inner">
-		<?php echo $text; ?>
+		<div class="vertical-align">
+			<?php echo $text; ?>
+		</div>
 	</div>
 </div>
