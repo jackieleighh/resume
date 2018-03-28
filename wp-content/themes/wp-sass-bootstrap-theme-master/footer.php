509081102f14773a7554d35bda5c1fcb9204d179
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-28 08:32:35 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/footer.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/footer.php
@@ -2,6 +2,7 @@
 					<div class="col-sm-12 section footer full left">
 						<div class="row inner">
 							<div class="inner-table">
+							<h2>keep in touch!</h2>
 							<?php 
 			                $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
 			                $menuID = $menuLocations['footer_menu']; // Get the *primary* menu ID
