3f9adb8f3011ed596c2b541f98f2d3a861e926ac
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-03-28 20:58:19 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/js/custom.js
+++ wp-content/themes/wp-sass-bootstrap-theme-master/js/custom.js
@@ -42,8 +42,10 @@ function slideBlock() {
 function navbarBrand() {
     if($(window).scrollTop() > 80) {
     	$('.navbar .navbar-brand').addClass('small');
+    	$('.navbar').addClass('opaque');
     } else {
     	$('.navbar .navbar-brand').removeClass('small');
+    	$('.navbar').removeClass('opaque');
     }
 }
 
