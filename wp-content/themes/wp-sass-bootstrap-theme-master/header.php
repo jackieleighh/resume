eb3061659d921c41b8406081b5e018c22fe92583
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-01 20:50:33 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/header.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/header.php
@@ -17,7 +17,7 @@
     <body <?php body_class(); ?>>
     
       <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
+        <div class="container-fluid container">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
