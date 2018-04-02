74233a1fdb7a7666820691ab18eed87462061dce
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-01 20:33:25 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
@@ -2,6 +2,6 @@
     $image = get_field('image');
 ?>
 
-<div class="col-sm-3 skill-section matchHeight">
+<div class="skill-section matchHeight">
 	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 </div>
