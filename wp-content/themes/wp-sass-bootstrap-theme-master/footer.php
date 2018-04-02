64f00f46cd8f033fb3508c71c9086b9f2a5f2c12
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-01 20:51:44 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/footer.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/footer.php
@@ -1,23 +1,25 @@
-				<section id="contact">
-					<div class="col-sm-12 section footer full left">
-						<div class="row inner">
-							<h2>let's get in touch!</h2>
-							<p>the tumblr account is just for fun :)</p>
-							<div class="inner-table">
-							<?php 
-			                $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
-			                $menuID = $menuLocations['footer_menu']; // Get the *primary* menu ID
-			                $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
+				<div class="container">
+					<section id="contact">
+						<div class="col-sm-12 section footer full left">
+							<div class="row inner">
+								<h2>let's get in touch!</h2>
+								<p>the tumblr account is just for fun :)</p>
+								<div class="inner-table">
+								<?php 
+				                $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
+				                $menuID = $menuLocations['footer_menu']; // Get the *primary* menu ID
+				                $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
 
-			                foreach ( $primaryNav as $navItem ) {
+				                foreach ( $primaryNav as $navItem ) {
 
-			                    echo '<div class="contact-link"><a href="'.$navItem->url.'" title="'.$navItem->title.'"><i class="'.$navItem->title.'"></i></a></div>';
+				                    echo '<div class="contact-link"><a href="'.$navItem->url.'" title="'.$navItem->title.'"><i class="'.$navItem->title.'"></i></a></div>';
 
-			                } ?>
-			                </div>
+				                } ?>
+				                </div>
+							</div>
 						</div>
-					</div>
-				</section>
+					</section>
+				</div>
 			</div>
 	    </div>
 	</div>
