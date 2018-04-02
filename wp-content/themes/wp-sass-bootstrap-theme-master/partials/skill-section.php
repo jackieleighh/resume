c975b998aa01bc19cc35c128aaee9ec4411ee91b
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-01 21:58:42 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
@@ -4,6 +4,6 @@
     $terms = get_the_terms($post->ID, 'skill-category');
 ?>
 
-<div class="col-sm-3 skill-section <?php foreach($terms as $term): echo $term->slug . ' '; endforeach; ?>">
+<div class="col-sm-3 matchHeight skill-section <?php foreach($terms as $term): echo $term->slug . ' '; endforeach; ?>">
 	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 </div>
