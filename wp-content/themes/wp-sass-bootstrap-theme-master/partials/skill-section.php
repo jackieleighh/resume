5b236780daa96879bbdb63de3ccd2b7fd9c5bc3c
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-03 20:55:36 -0400

    post types

--- wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
+++ wp-content/themes/wp-sass-bootstrap-theme-master/partials/skill-section.php
@@ -6,8 +6,10 @@
 
 <div class="col-sm-3 skill-section <?php foreach($terms as $term): echo $term->slug . ' '; endforeach; ?>">
 	<div class="inner">
-		<div class="vertical-align">
-			<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+		<div class="inner-table">
+			<div class="vertical-align">
+				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+			</div>
 		</div>
 	</div>
 </div>
