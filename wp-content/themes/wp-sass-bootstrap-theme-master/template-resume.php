a83921380c9b728d68f1a66ed2ddbd1acfe022ab
Jacqueline Harnish <jacquelineharnish@Jacquelines-MacBook-Pro.local>
2018-04-08 21:40:42 -0400

    pdf viewer plugin

---
+++ wp-content/themes/wp-sass-bootstrap-theme-master/template-resume.php
@@ -0,0 +1,20 @@
+
+<?php /* Template Name: Resume Template */ ?>
+
+<?php get_header(); ?>
+
+    <?php // theloop
+
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <?php echo get_the_content(); ?>
+    
+    <?php endwhile; ?>
+
+    <?php else: ?>
+
+        <?php get_404_template(); ?>
+
+    <?php endif; ?>
+
+<?php get_footer(); ?>      
