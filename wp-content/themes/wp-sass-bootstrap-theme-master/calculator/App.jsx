d4411d5daa7196f164f6d43acb1385d864bfa839
jharnish <jackie.harnish@zapsolutions.com>
2018-09-06 11:05:05 -0400

    fixing calculator logic

--- wp-content/themes/wp-sass-bootstrap-theme-master/calculator/App.jsx
+++ wp-content/themes/wp-sass-bootstrap-theme-master/calculator/App.jsx
@@ -97,6 +97,9 @@ class Calculator extends React.Component {
 			case '/':
 				res = this.state.previousNum / num;
 				break;
+			case '=':
+				res = parseFloat(this.state.result);
+				break;
 		}
 		this.setState({ result: res, previousNum: res, currentNum: null });
 	}
@@ -148,10 +151,9 @@ class Calculator extends React.Component {
 		});
 	}
 	changeSign() {
-		this.calculateResult();
 		this.setState({
-			result: this.state.result*-1, 
-			previousNum: this.state.previousNum*-1
+			result: -1*this.state.result, 
+			currentNum: -1*this.state.currentNum
 		});
 	}
 	render() {
